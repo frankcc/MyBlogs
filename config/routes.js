cacfff9b3e4e4261b5eea013731b90efbfc1d54c
frankcc <475491306@qq.com>
2015-04-11 23:14:51 +0800

    en
    en

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,18 @@
 module.exports = function(app) {
-	app.get('/',function(req,res){
-	res.render('index')
-	console.log('test')
-})
+	app.get('/', function(req, res) {
+		res.render('index')
+		console.log('test')
+	})
+	app.get('/admin/', function(req, res) {
+		res.render('admin/index')
+		console.log('test')
+	})
+	app.get('/admin/account', function(req, res) {
+		res.render('admin/account')
+		console.log('test')
+	})
+	app.get('/admin/article', function(req, res) {
+		res.render('admin/article')
+		console.log('test')
+	})
 }
---
+++ proxy/index.js
@@ -0,0 +1,5 @@
+exports.User = require('./user');
+exports.Comment = require('./comment');
+exports.Article = require('./article');
+exports.Chat = require('./chat');
+exports.Category = require('./category');
